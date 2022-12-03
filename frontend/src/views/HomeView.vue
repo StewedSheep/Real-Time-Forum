ee0b879c3f73fcbe0260055e87e6ca8ab0cea735
student <sander.vali.03@gmail.com>
2022-12-03 22:17:03 +0200

    home view rendering fixed

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -8,38 +8,45 @@ export default {
   data (){
     return {
       Threads: [],
+      cate: '',
     };
+  },
+  computed: {
+    sortPOST(){
+      return this.Threads.filter((t) => {
+        let categor = this.cate
+        if("My-Posts"==categor){
+          return t.author === this.$user.current
+        }else{return t.category.includes(categor)}
+     })
+    },
+    },
+    methods:{
+    listAll(){
+      this.cate = ''
+    },
+    listGen(){
+      this.cate = "General";
+    },
+    listHel(){
+      this.cate = "Help";
+    },
+    listNew(){
+      this.cate = "News";
+    },
+    listDis(){
+      this.cate = "Discussion";
+    },
+    listOff(){
+      this.cate = "Off-Topic"
+    },
+    listMyP(){
+      this.cate = "My-Posts"
+    },
   },
   mounted() {
           Vue.axios.get("/totThread").then((response) => (this.Threads = response.data));
     },
-    methods:{
-    liAll: function(){
-      this.cate = ""
-    },
-    liGen: function(){
-      this.cate = "General"
-    },
-    liHel: function(){
-      this.cate = "Help"
-    },
-    liNew: function(){
-      this.cate = "News"
-    },
-    liDis: function(){
-      this.cate = "Discussion"
-    },
-    liOff: function(){
-      this.cate = "Off-Topic"
-    },
-    liLik: function(){
-      // this.cate = "Liked-Posts" QOTD: HOW TO FILTER LIKED POSTS 
-    },
-    liMyP: function(){
-      this.cate = "My-Posts"
-      this.auth = this.$user.current
-    },
-  }
 }
 </script>
 
@@ -48,12 +55,12 @@ export default {
       <div v-if="!$user.isAuthorised">
           <p>Sort posts by:</p>
         <div class="btn-group" style="width:100%">
-          <button type="button" @click="liAll()" style="width:16.66%">All</button>
-          <button type="button" @click="liGen()" style="width:16.66%">General</button>
-          <button type="button" @click="liHel()" style="width:16.66%">Help</button>
-          <button type="button" @click="liNew()" style="width:16.66%">News</button>
-          <button type="button" @click="liDis()" style="width:16.66%">Discussion</button>
-          <button type="button" @click="liOff()" style="width:16.66%">Off-Topic</button>
+          <button type="button" @click="listAll()" style="width:16.66%">All</button>
+          <button type="button" @click="listGen()" style="width:16.66%">General</button>
+          <button type="button" @click="listHel()" style="width:16.66%">Help</button>
+          <button type="button" @click="listNew()" style="width:16.66%">News</button>
+          <button type="button" @click="listDis()" style="width:16.66%">Discussion</button>
+          <button type="button" @click="listOff()" style="width:16.66%">Off-Topic</button>
         </div>
       <br><br>
       </div>
@@ -61,40 +68,24 @@ export default {
       <div v-if="$user.isAuthorised">
           <p>Sort posts by:</p>
       <div class="btn-group" style="width:100%">
-          <button type="button" @click="liAll()" style="width:14.28%">All</button>
-          <button type="button" @click="liGen()" style="width:14.28%">General</button>
-          <button type="button" @click="liHel()" style="width:14.28%">Help</button>
-          <button type="button" @click="liNew()" style="width:14.28%">News</button>
-          <button type="button" @click="liDis()" style="width:14.28%">Discussion</button>
-          <button type="button" @click="liOff()" style="width:14.28%">Off-Topic</button>
-          <button type="button" @click="liMyP()" style="width:14.28%">My-Posts</button>
+          <button type="button" @click="listAll()" style="width:14.28%">All</button>
+          <button type="button" @click="listGen()" style="width:14.28%">General</button>
+          <button type="button" @click="listHel()" style="width:14.28%">Help</button>
+          <button type="button" @click="listNew()" style="width:14.28%">News</button>
+          <button type="button" @click="listDis()" style="width:14.28%">Discussion</button>
+          <button type="button" @click="listOff()" style="width:14.28%">Off-Topic</button>
+          <button type="button" @click="listMyP()" style="width:14.28%">My-Posts</button>
         </div>
         <br><br>
         </div>
 
     <div id="threadList">
-    <div v-if="Threads != null">
-      <div class="thread" v-for="Thread in Threads" :key="Thread">
-        <li v-if="Thread.category==cate">
-          <h1>{{ Thread.title }}</h1>
-          <p>Category:{{ Thread.category }} Author:{{ Thread.author }}
-          Date:{{ Thread.date }} </p>
-          <p>{{ Thread.content }}</p>
-        </li>
-        <li v-else-if="'My-Posts'==cate && Thread.author==auth">
-          <h1>{{ Thread.title }}</h1>
-          <p>Category:{{ Thread.category }} Author:{{ Thread.author }}
-          Date:{{ Thread.date }} </p>
-          <p>{{ Thread.content }}</p>
-        </li>
-        <li v-else-if="!cate && !auth">
-          <h1>{{ Thread.title }}</h1>
-          <p>Category:{{ Thread.category }} Author:{{ Thread.author }}
-          Date:{{ Thread.date }} </p>
-          <p>{{ Thread.content }}</p>      
-        </li>
-    </div>
-    </div>
+      <div class="thread" v-for="thread in sortPOST" v-bind:key=thread>
+        <h1>{{ thread.title }}</h1>
+          <p>Category:{{ thread.category }} Author:{{ thread.author }}
+          Date:{{ thread.date }} </p>
+          <p>{{ thread.content }}</p>  
+          </div>
     </div>
 </div>
 </template>
