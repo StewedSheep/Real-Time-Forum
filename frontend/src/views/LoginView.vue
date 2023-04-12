42e9470af2a594fc9c38e37702256fd2fdc28fd4
KristjanL <kristjan.lepikult@gmail.com>
2023-04-12 08:43:48 +0000

    Login error fix

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -13,18 +13,19 @@ export default {
         }
     },
     methods : {
-        postLog: function() {
+      postLog: function() {
       var data = {username: this.username,
                   password: this.password,
-                }
-  console.log(data)
+                  LoginError: this.LoginError,
+      }
+      console.log(data)
       Vue.axios.post('/login', data, {
-    headers: {
+        headers: {
           'Content-Type': 'text/plain',
           'Access-Control-Allow-Origin': '*'
-            }
-        }).then((response) => (this.LoginError = response.data));
-        if (this.LoginError == ""){this.$router.push("/")}
+        }
+      }).then((response) => (this.LoginError = response.data));
+      if (this.LoginError == "Check login info."){this.$router.push("/")}
       },
     }
 }
@@ -33,33 +34,27 @@ export default {
 
 <template>
 <div v-if="$user.isAuthorised">
-  <h3>Already Logged in. Please return to the home page.</h3>
+  <h3 class="thread" style="text-align: center;">Already Logged in. Please return to the home page.</h3>
 </div>
 <div v-else id="app">
-    <center>
-        <h3>Login here</h3>
-
-        <div v-if="LoginError != null">
+  <center>
+  <h3>Login here</h3>
+  <div v-if="LoginError != null">
     <p>{{ LoginError }}</p>
-
     <br/>
-    </div> 
-
-<form>
-            <label>Username or Email:</label><br>
-            <input class="logButton" type="text" v-model="username" name="username" /><br><br>
-
-            <label for="password">Password:</label><br>
-            <input  class="logButton" type="password" v-model="password" name="password" /><br><br>
-
-            <br>
-          </form> 
-
-            <div>
-      <button class="logButton" type="button" @click="postLog()">Login</button>  <br><br>
+  </div> 
+  <form>
+    <label>Username or Email:</label><br>
+    <input class="logButton" type="text" v-model="username" name="username" /><br><br>
+    <label for="password">Password:</label><br>
+    <input  class="logButton" type="password" v-model="password" name="password" /><br><br>
+    <br>
+  </form> 
+  <div>
+    <button class="logButton" type="button" @click="postLog()">Login</button>  <br><br>
   <router-link to="register"><img class="noAccount" src="@/assets/no_account.jpg" alt="Click here to register!"></router-link>
-    </div>
-<router-view />
-</center>
+  </div>
+  <router-view />
+  </center>
 </div>
 </template>
