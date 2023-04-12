f22384d55eb3690237b9e28d3526124d7c2e44c3
KristjanL <kristjan.lepikult@gmail.com>
2023-04-12 09:33:03 +0000

    some stylising fixes

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="app">
+  <div style="color:#282F44 ;" class="notFoundPage" id="app">
     <h1>Not Found</h1>
     <p>
       We couldn't find that page. Try going to the home page.
-      <router-link to="/">home</router-link>
+      <router-link class="notFoundPageHomeButton" to="/">home</router-link>
     </p>
   </div>
 </template>
